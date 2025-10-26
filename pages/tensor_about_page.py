dbd1874a104a7ceeffaba59ed7ca0642621239bc
sasha <trueotmor@gmail.com>
2025-10-27 02:45:16 +0300

    Рефакторинг tensor_main_page.py и tensor_about_page.py: удалены неиспользуемые импорты, упрощены методы, добавлена документация

--- pages/tensor_about_page.py
+++ pages/tensor_about_page.py
@@ -1,17 +1,14 @@
 from selenium.webdriver.common.by import By
 from .base_page import BasePage
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 class TensorAboutPage(BasePage):
+    """Page Object для страницы 'О тензоре'"""
+    
     WORKING_IMAGES = (By.CSS_SELECTOR, "img.tensor_ru-About__block3-image")
 
     def __init__(self, driver):
         super().__init__(driver)
 
     def find_working_images(self):
-        WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located(self.WORKING_IMAGES)
-        )
-        images = self.driver.find_elements(*self.WORKING_IMAGES)
-        return images
+        """Поиск изображений в разделе 'Работаем'"""
+        return self.driver.find_elements(*self.WORKING_IMAGES)
--- pages/tensor_main_page.py
+++ pages/tensor_main_page.py
@@ -1,9 +1,9 @@
 from selenium.webdriver.common.by import By
 from .base_page import BasePage
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 class TensorMainPage(BasePage):
+    """Page Object для главной страницы Tensor"""
+    
     POWER_BLOCK = (By.XPATH, "//p[@class='tensor_ru-Index__card-title tensor_ru-pb-16' and contains(text(), 'Сила в людях')]")
     DETAILS_LINK = (By.XPATH, "//a[@href='/about' and contains(text(), 'Подробнее')]")
 
@@ -11,11 +11,10 @@ class TensorMainPage(BasePage):
         super().__init__(driver)
 
     def find_power_block(self):
-        WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located(self.POWER_BLOCK)
-        )
-        return self.driver.find_element(*self.POWER_BLOCK)
+        """Поиск блока 'Сила в людях'"""
+        return self.find_element(self.POWER_BLOCK, timeout=10)
 
     def click_details(self):
+        """Клик по ссылке 'Подробнее'"""
         self.click_element(self.DETAILS_LINK)
         self.wait_for_url_contains("/about", timeout=10)
